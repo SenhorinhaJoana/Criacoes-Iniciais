89565d616d1b201e403c9ea596aa963bcdfd842c
senhorinhajoana <30864632-senhorinhajoana@users.noreply.replit.com>
2024-01-03 01:07:22 +0000

    Initial commit

---
+++ exercíco1.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    double volume, comprimento, largura, altura;
+    Console.Write("Digite o comprimento da caixa:");
+    comprimento = double.Parse(Console.ReadLine());
+    Console.Write( "Digite a largura da caixa:");
+    largura = double.Parse(Console.ReadLine());
+    Console.Write("Digite a altura da caixa:");
+    altura = double.Parse(Console.ReadLine());
+    Console.Write( "O volume da caixa é:");
+    volume = comprimento * largura * altura;
+    Console.WriteLine($"O volume da caixa com {comprimento} metros de comprimentos, {largura} metros de largura {altura} metros de altura é: {volume} metros cúbicos.");
+    
+    
+    }
+  }
